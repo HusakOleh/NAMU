f883313e783254b405d64ccc6e8e585e60f342c9
HusakOleh <husakoleh91@gmail.com>
2022-09-03 17:32:54 +0300

    finish site

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,48 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__menu-on');
+  } else {
+    document.body.classList.remove('page__menu-on');
+  }
+});
+
+function stopDefAction(evt) {
+  evt.preventDefault();
+}
+
+document.querySelector('#button').addEventListener(
+  'click', stopDefAction, false
+);
+
+function resField(evt) {
+  document.querySelector('#email').value = '';
+}
+
+document.querySelectorAll('#send').addEventListener(
+  'click', resField, false
+);
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
